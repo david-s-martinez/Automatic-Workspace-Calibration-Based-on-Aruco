b063205c21a3f126f953cb3fc49dbabb4ceecac0
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2022-06-23 21:04:42 +0200

    added predefined tag cubes to constructor and renamed functions

--- aruco_3d_improved.py
+++ aruco_3d_improved.py
@@ -4,7 +4,7 @@ import cv2.aruco as aruco
 import sys, time, math
 
 class PlaneDetection:
-    def __init__(self, calib_path):
+    def __init__(self, calib_path, grid_w, grid_h,):
         """
         PlaneDetection object constructor. Initializes data containers.
         
@@ -18,6 +18,54 @@ class PlaneDetection:
         
         self.cube_vertices = {}
         self.homography = None
+
+        self.tag_cubes = {
+            '0': np.array([
+                0, 0, 0,
+                grid_w, 0, 0,
+                grid_w, -grid_h, 0,
+                0, -grid_h, 0,
+                0, 0, 3,
+                grid_w, 0, 3,
+                grid_w, -grid_h, 3,
+                0, -grid_h, 3]),
+            '1': np.array([
+                0, 0, 0,
+                0, -grid_h, 0,
+                -grid_w, -grid_h, 0,
+                -grid_w, 0, 0,
+                0, 0, 3,
+                0, -grid_h, 3,
+                -grid_w, -grid_h, 3,
+                -grid_w, 0, 3]),
+            '2': np.array([
+                0, 0, 0,
+                -grid_w, 0, 0,
+                -grid_w, grid_h, 0,
+                0, grid_h, 0,
+                0, 0, 3,
+                -grid_w, 0, 3,
+                -grid_w, grid_h, 3,
+                0, grid_h, 3]),
+            '3': np.array([
+                0, 0, 0,
+                0, grid_h, 0,
+                grid_w, grid_h, 0,
+                grid_w, 0, 0,
+                0, 0, 3,
+                0, grid_h, 3,
+                grid_w, grid_h, 3,
+                grid_w, 0, 3]),
+            '4': np.array([
+                -12.75, 7, 0,
+                12.75, 7, 0,
+                12.75, -7, 0,
+                -12.75, -7, 0,
+                -12.75, 7, 3,
+                12.75, 7, 3,
+                12.75, -7, 3,
+                -12.75, -7, 3])
+            }
 
     def draw_tag_pose(self,image, rvec, tvec, z_rot=-1):
         world_points = np.array([
@@ -44,64 +92,18 @@ class PlaneDetection:
         cv2.putText(image, str((0,0)), (img_points[1][0]+10,img_points[1][1]-30), font, 0.5,
                                         (255, 255, 255), 1, cv2.LINE_AA)
 
-    def define_world_pts(self,iD,grid_w, grid_h):
-        tag_cubes = {'0': np.array([
-                        0, 0, 0,
-                        grid_w, 0, 0,
-                        grid_w, -grid_h, 0,
-                        0, -grid_h, 0,
-                        0, 0, 3,
-                        grid_w, 0, 3,
-                        grid_w, -grid_h, 3,
-                        0, -grid_h, 3]),
-                    '1': np.array([
-                        0, 0, 0,
-                        0, -grid_h, 0,
-                        -grid_w, -grid_h, 0,
-                        -grid_w, 0, 0,
-                        0, 0, 3,
-                        0, -grid_h, 3,
-                        -grid_w, -grid_h, 3,
-                        -grid_w, 0, 3]),
-                    '2': np.array([
-                        0, 0, 0,
-                        -grid_w, 0, 0,
-                        -grid_w, grid_h, 0,
-                        0, grid_h, 0,
-                        0, 0, 3,
-                        -grid_w, 0, 3,
-                        -grid_w, grid_h, 3,
-                        0, grid_h, 3]),
-                    '3': np.array([
-                        0, 0, 0,
-                        0, grid_h, 0,
-                        grid_w, grid_h, 0,
-                        grid_w, 0, 0,
-                        0, 0, 3,
-                        0, grid_h, 3,
-                        grid_w, grid_h, 3,
-                        grid_w, 0, 3]),
-                    '4': np.array([
-                        12.75, 7, 0,
-                        -12.75, 7, 0,
-                        -12.75, -7, 0,
-                        12.75, -7, 0,
-                        12.75, 7, 3,
-                        -12.75, 7, 3,
-                        -12.75, -7, 3,
-                        12.75, -7, 3])
-                    }
-        world_points = tag_cubes[str(iD)].reshape(-1, 1, 3)
+    def define_world_pts(self,iD):
+        world_points = self.tag_cubes[iD].reshape(-1, 1, 3)
         return world_points * 0.5 * self.marker_size
+
     def draw_cube(self,image, iD , rvec, tvec):
-        grid_w = 25.5
-        grid_h = 14.0
-        world_points = self.define_world_pts(iD, grid_w, grid_h, self.marker_size)
+
+        world_points = self.define_world_pts(str(iD), self.marker_size)
         img_points, _ = cv2.projectPoints(world_points, rvec, tvec, self.camera_matrix, self.camera_distortion)
         img_points = np.round(img_points).astype(int)
         # print(img_points)
         img_points = [tuple(pt) for pt in img_points.reshape(-1, 2)] # -> [(x1,y1),(x2,y2),...] in pixels
-        
+
         cv2.line(image, img_points[0], img_points[1], (255,0,0), 2)
         cv2.line(image, img_points[1], img_points[2], (255,0,0), 2)
         cv2.line(image, img_points[2], img_points[3], (255,0,0), 2)
@@ -115,7 +117,7 @@ class PlaneDetection:
         cv2.line(image, img_points[2], img_points[6], (255,0,0), 2)
         cv2.line(image, img_points[3], img_points[7], (255,0,0), 2)
 
-    def tag_z_vertices(self, rvec, tvec, z_rot=-1):
+    def compute_tag_z_vertices(self, rvec, tvec, z_rot=-1):
         world_points = np.array([
             0, 0, 0,
             0, 0, -3 * z_rot
@@ -126,7 +128,7 @@ class PlaneDetection:
         img_points = [tuple(pt) for pt in img_points.reshape(-1, 2)]
 
         return img_points[0],img_points[1]
-    def update_img_pts(self,iD,grid_w, grid_h, cube_vertices, rvec, tvec):
+    def update_img_pts(self,iD, cube_vertices, rvec, tvec):
         
         points_update = [None,None,None,None,None,None,None,None]
         vert_2_update = list(cube_vertices.keys())
@@ -141,16 +143,6 @@ class PlaneDetection:
             if '3' in vert_2_update:
                 points_update[3] = cube_vertices['3'][0]
                 points_update[7] = cube_vertices['3'][1]
-            world_points = np.array([
-                    0, 0, 0,
-                    grid_w, 0, 0,
-                    grid_w, -grid_h, 0,
-                    0, -grid_h, 0,
-                    0, 0, 3,
-                    grid_w, 0, 3,
-                    grid_w, -grid_h, 3,
-                    0, -grid_h, 3
-                ]).reshape(-1, 1, 3)
             
         if iD == '1':
             if '2' in vert_2_update:
@@ -162,16 +154,6 @@ class PlaneDetection:
             if '0' in vert_2_update:
                 points_update[3] = cube_vertices['0'][0]
                 points_update[7] = cube_vertices['0'][1]
-            world_points = np.array([
-                    0, 0, 0,
-                    0, -grid_h, 0,
-                    -grid_w, -grid_h, 0,
-                    -grid_w, 0, 0,
-                    0, 0, 3,
-                    0, -grid_h, 3,
-                    -grid_w, -grid_h, 3,
-                    -grid_w, 0, 3
-                ]).reshape(-1, 1, 3)
             
         if iD == '2':
             if '3' in vert_2_update:
@@ -183,16 +165,6 @@ class PlaneDetection:
             if '1' in vert_2_update:
                 points_update[3] = cube_vertices['1'][0]
                 points_update[7] = cube_vertices['1'][1]
-            world_points = np.array([
-                    0, 0, 0,
-                    -grid_w, 0, 0,
-                    -grid_w, grid_h, 0,
-                    0, grid_h, 0,
-                    0, 0, 3,
-                    -grid_w, 0, 3,
-                    -grid_w, grid_h, 3,
-                    0, grid_h, 3,
-                ]).reshape(-1, 1, 3)
             
         if iD == '3':
             if '0' in vert_2_update:
@@ -204,17 +176,6 @@ class PlaneDetection:
             if '2' in vert_2_update:
                 points_update[3] = cube_vertices['2'][0]
                 points_update[7] = cube_vertices['2'][1]
-            world_points = np.array([
-                    0, 0, 0,
-                    0, grid_h, 0,
-                    grid_w, grid_h, 0,
-                    grid_w, 0, 0,
-                    0, 0, 3,
-                    0, grid_h, 3,
-                    grid_w, grid_h, 3,
-                    grid_w, 0, 3
-                    
-                ]).reshape(-1, 1, 3)
             
         if iD == '4':
             if '0' in vert_2_update:
@@ -229,19 +190,8 @@ class PlaneDetection:
             if '3' in vert_2_update:
                 points_update[3] = cube_vertices['3'][0]
                 points_update[7] = cube_vertices['3'][1]
-            world_points = np.array([
-                    -12.75, 7, 0,
-                    12.75, 7, 0,
-                    12.75, -7, 0,
-                    -12.75, -7, 0,
-                    -12.75, 7, 3,
-                    12.75, 7, 3,
-                    12.75, -7, 3,
-                    -12.75, -7, 3
-                    
-                ]).reshape(-1, 1, 3)
 
-        world_points = world_points * 0.5 * self.marker_size
+        world_points = self.define_world_pts(iD)
         img_points, _ = cv2.projectPoints(world_points, rvec, tvec, self.camera_matrix, self.camera_distortion)
         img_points = np.round(img_points).astype(int)
         img_points = [ points_update[i] if points_update[i] else tuple(pt) for i, pt in enumerate(img_points.reshape(-1, 2))] # -> [(x1,y1),(x2,y2),...] in pixels
@@ -249,10 +199,9 @@ class PlaneDetection:
         return img_points
 
     def draw_cube_update(self,image, iD, cube_vertices, rvec, tvec):
-        grid_w = 23.5
-        grid_h = 13.0
+        
         # print(iD)
-        img_points = self.update_img_pts(str(iD), grid_w, grid_h, cube_vertices, rvec, tvec)
+        img_points = self.update_img_pts(str(iD), cube_vertices, rvec, tvec)
 
         cv2.line(image, img_points[0], img_points[1], (255,0,0), 2)
         cv2.line(image, img_points[1], img_points[2], (255,0,0), 2)
@@ -287,7 +236,7 @@ class PlaneDetection:
             cv2.aruco.drawDetectedMarkers(frame, corners)
             grid_id = ids[0][0]
             self.rot_vecs, self.tran_vecs = poses[0], poses[1]
-            self.cube_vertices = {str(tag_id[0]):pd.tag_z_vertices( 
+            self.cube_vertices = {str(tag_id[0]):pd.compute_tag_z_vertices( 
                                                             self.rot_vecs[i][0], 
                                                             self.tran_vecs[i][0]) 
                                                             for i, tag_id in enumerate(ids)}
@@ -303,8 +252,11 @@ class PlaneDetection:
             del self.cube_vertices
 	
 calib_path = ""
-
-pd = PlaneDetection(calib_path)
+# grid_w = 23.5
+# grid_h = 13.0
+grid_w = 25.5
+grid_h = 14.0
+pd = PlaneDetection(calib_path, grid_w, grid_h)
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
