e8b813f94e462175fc7dd8462b3c0a64b1cb4a80
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2022-06-28 15:12:43 +0200

    cleaned code

--- aruco_3d_improved.py
+++ aruco_3d_improved.py
@@ -121,16 +121,6 @@ class PlaneDetection:
             return self.homography
     
     def compute_perspective_trans(self, image):
-        if self.homography is not None:
-            height, width, channels = image.shape
-            im1Reg = cv2.warpPerspective(image, self.homography, (width, height))
-            return im1Reg
-        else:
-            return image
-
-    def four_point_transform(self, image):
-        # obtain a consistent order of the points and unpack them
-        # individually
         corners = ['0','1','2','3']
         height, width, channels = image.shape
         
@@ -146,31 +136,15 @@ class PlaneDetection:
             widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
             widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
             maxWidth = max(int(widthA), int(widthB))
-            # compute the height of the new image, which will be the
-            # maximum distance between the top-right and bottom-right
-            # y-coordinates or the top-left and bottom-left y-coordinates
+
             heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
             heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
             maxHeight = max(int(heightA), int(heightB))
-            # now that we have the dimensions of the new image, construct
-            # the set of destination points to obtain a "birds eye view",
-            # (i.e. top-down view) of the image, again specifying points
-            # in the top-left, top-right, bottom-right, and bottom-left
-            # order
-            # dst = np.array([
-            #     [0, 0],
-            #     [maxWidth - 1, 0],
-            #     [maxWidth - 1, maxHeight - 1],
-            #     [0, maxHeight - 1]], dtype = "float32")
-            # # compute the perspective transform matrix and then apply it
-            # M = cv2.getPerspectiveTransform(rect, dst)
+           
             warped = cv2.warpPerspective(image, self.homography, (maxWidth, maxHeight))
-            # warped = cv2.resize(warped, (width, height))
-            # return the warped image
+            
             return warped
         else:
-            # warped = np.zeros_like(image)
-            # return warped
             return None
 
     def draw_tag_pose(self,image, rvec, tvec, z_rot=-1):
@@ -398,8 +372,7 @@ while True:
     pd.detect_tags_3D(frame)
     print(pd.cube_vertices)
     homography = pd.compute_homog()
-    print(homography)
-    frame_persp_trans = pd.four_point_transform(frame_warp)
+    frame_persp_trans = pd.compute_perspective_trans(frame_warp)
             
     cv2.imshow('frame', frame)
     if frame_persp_trans is not None:
